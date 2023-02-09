503c422635dc332b15c0535d199276696b14e1a2
Seth Bowman <brandonsethbowman@gmail.com>
2023-02-09 10:27:21 -0600

    Called api using RestSharp, printed 5 jokes to the console

---
+++ JiveAPI/Program.cs
@@ -0,0 +1,10 @@
+﻿using RestSharp;
+using Newtonsoft.Json;
+
+
+for (int i = 1; i <= 5; i++)
+{
+    var joke = Root.GetJoke();
+    Console.WriteLine($"Chuck Norris Joke #{i}: {joke.Value}");
+    Console.WriteLine();
+}
---
+++ JiveAPI/Root.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+
+public class Root
+{
+    
+    public static Root GetJoke()
+    {
+        var key = File.ReadAllText("appsettings.json");
+        var apiKey = JObject.Parse(key).GetValue("DefaultKey").ToString();
+
+        var client = new RestClient("https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random");
+        var request = new RestRequest();
+        request.AddHeader("accept", "application/json");
+        request.AddHeader("X-RapidAPI-Key", $"{apiKey}");
+        request.AddHeader("X-RapidAPI-Host", "matchilling-chuck-norris-jokes-v1.p.rapidapi.com");
+        var response = client.Execute(request);
+
+        var root = JsonConvert.DeserializeObject<Root>(response.Content);
+
+        return root;
+    }
+
+    [JsonProperty("categories")]
+    public List<object>? Categories { get; set; }
+
+    [JsonProperty("created_at")]
+    public string? CreatedAt { get; set; }
+
+    [JsonProperty("icon_url")]
+    public string? IconUrl { get; set; }
+
+    [JsonProperty("id")]
+    public string? Id { get; set; }
+
+    [JsonProperty("updated_at")]
+    public string? UpdatedAt { get; set; }
+
+    [JsonProperty("url")]
+    public string? Url { get; set; }
+
+    [JsonProperty("value")]
+    public string? Value { get; set; }
+}
